3364fa8864eb60a373dc18f09acdb10aefaee475
adrien matta <matta@lpccaen.in2p3.fr>
2017-03-21 10:16:48 +0100

    * adding my own planck layout         - Initial commit with some setting up of my variable

---
+++ keyboards/planck/keymaps/adrien/keymap.c
@@ -0,0 +1,151 @@
+// This is the canonical layout file for the Quantum project. If you want to add another keyboard,
+// this is the style you want to emulate.
+
+#include "planck.h"
+#include "action_layer.h"
+#ifdef AUDIO_ENABLE
+  #include "audio.h"
+#endif
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
+
+enum planck_layers {
+  L_QWERTY,
+  L_0,
+  L_1,
+  L_2,
+  L_3,
+  L_4,
+  L_F,
+  L_Nav
+};
+
+enum planck_keycodes {
+  KL_QWERTY = SAFE_RANGE,
+  KL_0,
+  KL_1,
+  KL_2,
+  KL_3,
+  KL_4,
+  KL_F,
+  KL_BL
+};
+
+// Fillers to make layering more clear
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+/* Qwerty
+ * ,-----------------------------------------------------------------------------------.
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
+ * |------+------+------+------+------+-------------+------+------+------+------+------|
+ * | KL0  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  | KL0  |
+ * |------+------+------+------+------+------|------+------+------+------+------+------|
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  | Shift|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * | Ctrl | Alt  | KL1  | KL2  |Esc   |    Space    |ENT   | KL3  | KL4  |  OS  |BL    |
+ * `-----------------------------------------------------------------------------------'
+ */
+[L_QWERTY] = {
+  {KC_TAB ,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC },
+  {KL_0   ,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KL_1     },
+  {KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT },
+  {KC_LCTL,  KC_LALT, KL_1,    KL_2,    KC_ESC,  KC_SPC,  KC_SPC,  KC_ENT,  KL_3,    KL_4,    KC_LGUI,  KL_BL}
+
+},
+
+[L_0] = {
+  {KC_TAB ,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_BSPC   },
+  {KL_0   ,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KL_0      },
+  {XXXXXXX,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  XXXXXXX   },
+  {KC_LCTL,  KC_LALT, XXXXXXX, XXXXXXX, KC_ESC,  KC_SPC,  KC_SPC,  KC_ENT,  XXXXXXX, XXXXXXX, KC_LGUI,  XXXXXXX   }
+
+}
+
+
+};
+
+#ifdef AUDIO_ENABLE
+
+float tone_startup[][2]    = SONG(STARTUP_SOUND);
+float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
+float tone_dvorak[][2]     = SONG(DVORAK_SOUND);
+float tone_colemak[][2]    = SONG(COLEMAK_SOUND);
+float tone_plover[][2]     = SONG(PLOVER_SOUND);
+float tone_plover_gb[][2]  = SONG(PLOVER_GOODBYE_SOUND);
+float music_scale[][2]     = SONG(MUSIC_SCALE_SOUND);
+
+float tone_goodbye[][2] = SONG(GOODBYE_SOUND);
+#endif
+
+
+void persistant_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case KL_QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+        #endif
+        persistant_default_layer_set(1UL<<L_QWERTY);
+      }
+      return false;
+      break;
+    case KL_0:
+      if (record->event.pressed) {
+        layer_on(L_0);
+        update_tri_layer(L_0, L_1, L_2);
+      } else {
+        layer_off(L_0);
+        update_tri_layer(L_0, L_1, L_2);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+void matrix_init_user(void) {
+    #ifdef AUDIO_ENABLE
+        startup_user();
+    #endif
+}
+
+#ifdef AUDIO_ENABLE
+
+void startup_user()
+{
+    _delay_ms(20); // gets rid of tick
+    PLAY_NOTE_ARRAY(tone_startup, false, 0);
+}
+
+void shutdown_user()
+{
+    PLAY_NOTE_ARRAY(tone_goodbye, false, 0);
+    _delay_ms(150);
+    stop_all_notes();
+}
+
+void music_on_user(void)
+{
+    music_scale_user();
+}
+
+void music_scale_user(void)
+{
+    PLAY_NOTE_ARRAY(music_scale, false, 0);
+}
+
+#endif
